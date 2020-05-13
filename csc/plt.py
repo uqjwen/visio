80120f3ebd6d7a0d9289c4423fe9cb8b4afa14c2
uqjwen <wen_jiahui@outlook.com>
2020-05-13 19:50:01 +0800

    wenjh

--- csc/plt.py
+++ csc/plt.py
@@ -19,33 +19,40 @@ def sample(data):
 
 def main():
 	fontsize = 15
-	data1 = np.genfromtxt('res_1.txt')
+	filename = ['res_'+str(i+1)+'.txt' for i in range(3)]
+	val      = [0.578, 0.681, 0.690]
+	for i,file in enumerate(filename):
 
-	x_data, y_data = sample(data1)
-	# x_data, y_data = data1[:,0], data1[:,1]
+		data = np.genfromtxt(file)
 
-	print(x_data)
 
-	plt.plot(x_data, y_data, label = 'fix', color='c', ls = '-.', marker = 'o', markersize = 8)
-	plt.plot(x_data, [0.578]*len(x_data), label = 'automatic', ls = '--',color = 'green', marker = 'p', markersize = 8)
+		x_data, y_data = sample(data)
+		# x_data, y_data = data1[:,0], data1[:,1]
 
-	x_major_locator=MultipleLocator(0.1)
-	y_major_locator=MultipleLocator(0.02)
+		# print(x_data)
 
-	ax = plt.gca()
+		plt.plot(x_data, y_data, label = 'fix', color='c', ls = '-.', marker = 'o', markersize = 10)
+		plt.plot(x_data, [val[i]]*len(x_data), label = 'automatic', ls = '--',color = 'green', marker = 'p', markersize = 10)
 
-	ax.xaxis.set_major_locator(x_major_locator)
-	ax.yaxis.set_major_locator(y_major_locator)
+		x_major_locator=MultipleLocator(0.1)
+		y_major_locator=MultipleLocator(0.02)
 
-	plt.xticks(fontsize = fontsize)
-	plt.yticks(fontsize = fontsize)
-	plt.xlabel(r'$\theta_s$', fontsize = fontsize)
-	plt.ylabel("Accuracy", fontsize = fontsize)
-	plt.legend(fontsize = fontsize)
-	plt.ylim(0.4,0.6)
-	# plt.xlim(0.1,1)
+		ax = plt.gca()
 
-	plt.show()
+		ax.xaxis.set_major_locator(x_major_locator)
+		ax.yaxis.set_major_locator(y_major_locator)
+
+		plt.xticks(fontsize = fontsize)
+		plt.yticks(fontsize = fontsize)
+		plt.xlabel(r'$\theta_s$', fontsize = fontsize)
+		plt.ylabel("Accuracy", fontsize = fontsize)
+		plt.legend(loc = 'lower center', fontsize = fontsize)
+		minimum = np.min(y_data)-0.05
+		maximum = np.max(y_data)+0.05
+		plt.ylim(minimum, maximum)
+		# plt.xlim(0.1,1)
+
+		plt.show()
 
 
 
